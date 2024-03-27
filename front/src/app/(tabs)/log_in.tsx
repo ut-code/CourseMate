5d12e23fd92d0f31acc73c126539679d82194fd2
GaiKOZAKI <125722953+GaiKOZAKI@users.noreply.github.com>
2024-03-27 12:23:24 +0900

    ログインとサインアップ画面を追加

---
+++ front/src/app/(tabs)/log_in.tsx
@@ -0,0 +1,76 @@
+import { Link } from "expo-router";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+
+import Button from "../../components/Button";
+
+const handlePress = (): void => {
+  // LogIn
+};
+
+const LogIn = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.inner}>
+        <Text style={styles.title}>Log In</Text>
+        <TextInput style={styles.input} value="Email" />
+        <TextInput style={styles.input} value="Password" />
+        <Button label="Submit" onPress={handlePress} />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not registered?</Text>
+          <Link href="/sign_up" asChild>
+            <TouchableOpacity>
+              <Text style={styles.footerLink}>Sign Up here!</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f0f4f8",
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: "#dddddd",
+    borderWidth: 1,
+    backgroundColor: "#ffffff",
+    padding: 8,
+    marginBottom: 16,
+  },
+  footer: {
+    flexDirection: "row",
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: "#467fd3",
+  },
+});
+
+export default LogIn;
