e34448debb6dd22f565fd60546ac71d7e9a7827b
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-04-17 20:36:44 +0900

    fix: index.jsのインポートを修正

--- back/src/index.js
+++ back/src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
-import userRoutes from "./routes/userRoutes.js";
-import followingRequestRoutes from "./routes/followingRequestRoutes.js";
-import courseRoutes from "./routes/courseRoutes.js";
-import enrollmentRoutes from "./routes/enrollmentRoutes.js";
-import sampleRoutes from "./routes/sampleRoutes.js";
+import userRoutes from "./routes/api/userRoutes.js";
+import followingRequestRoutes from "./routes/api/followingRequestRoutes.js";
+import courseRoutes from "./routes/api/courseRoutes.js";
+import enrollmentRoutes from "./routes/api/enrollmentRoutes.js";
+import sampleRoutes from "./routes/api/sampleRoutes.js";
+import relationshipRoutes from "./routes/api/relationshipRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -23,6 +24,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/followingRequests", followingRequestRoutes);
 app.use("/api/courses", courseRoutes);
 app.use("/api/enrollment", enrollmentRoutes);
+app.use("/api/relationship", relationshipRoutes);
 
 // サンプル
 app.use("/api/sample", sampleRoutes);
