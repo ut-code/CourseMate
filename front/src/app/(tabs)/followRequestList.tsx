fcee9935d3a4f40fd835f62832a8d7a9336223a7
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-05-26 13:03:26 +0900

    fetchの時のエラーの処理を追加

--- front/src/app/(tabs)/followRequestList.tsx
+++ front/src/app/(tabs)/followRequestList.tsx
@@ -25,6 +25,17 @@ fetch("http://localhost:3000/requests/" + currentUserId.toString(), {
   .then((data) => {
     console.log("success: fetching matchRequests");
     matchRequests = data;
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // The email of the user's account used.
+    // const email = error.customData.email;
+    // The AuthCredential type that was used.
+    // const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    console.error(error);
   });
 
 const List = (): JSX.Element => {
