05a5ac8d0eb4810b53ef74f9071cd7897f2550bb
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-05-28 18:52:56 +0900

    サインインボタンを非同期処理に

--- front/src/app/login.tsx
+++ front/src/app/login.tsx
@@ -7,33 +7,32 @@ import { auth } from "../firebase/firebaseconfig";
 
 const provider = new GoogleAuthProvider();
 
-const signIn = (): void => {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      if (credential) {
-        // const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
-        console.log(user.metadata);
-        console.log("ログインに成功しました");
-      }
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      // const errorCode = error.code;
-      // const errorMessage = error.message;
-      // The email of the user's account used.
-      // const email = error.customData.email;
-      // The AuthCredential type that was used.
-      // const credential = GoogleAuthProvider.credentialFromError(error);
+const signIn = async (): Promise<void> => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    if (credential) {
+      // const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
       // ...
-      console.error(error);
-      console.log("ログインに失敗しました");
-    });
+      console.log(user.metadata);
+      console.log("ログインに成功しました");
+    }
+  } catch (error) {
+    // Handle Errors here.
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // The email of the user's account used.
+    // const email = error.customData.email;
+    // The AuthCredential type that was used.
+    // const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    console.error(error);
+    console.log("ログインに失敗しました");
+  }
 };
 
 const LogIn = (): JSX.Element => {
@@ -45,8 +44,8 @@ const LogIn = (): JSX.Element => {
         <Text style={styles.title}>Log In</Text>
         <Button
           label="Log In"
-          onPress={() => {
-            signIn();
+          onPress={async () => {
+            await signIn();
             router.push("/");
           }}
         />
