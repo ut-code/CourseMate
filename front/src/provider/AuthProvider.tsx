2db61e65febc9d1b98a4bf69b258e54b1472663a
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-06-01 11:07:49 +0900

    模索中

--- front/src/provider/AuthProvider.tsx
+++ front/src/provider/AuthProvider.tsx
@@ -1,5 +1,5 @@
-import { useRouter } from "expo-router";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "expo-router";
 import React, { createContext, useContext, useEffect, useState } from "react";
 
 interface User {
@@ -29,22 +29,58 @@ export default function AuthProvider({
     }
   }
 
+  async function registerNewUser(firebaseUser: any) {
+    try {
+      const response = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          uid: firebaseUser.uid,
+          name: firebaseUser.displayName,
+          email: firebaseUser.email,
+        }),
+      });
+      console.log(response);
+
+      if (!response.ok) {
+        throw new Error('Failed to register new user');
+      }
+
+      const newUser = await response.json();
+      setUser(newUser);
+    } catch (error) {
+      console.error('Error registering new user:', error);
+    }
+  }
+
   useEffect(() => {
     try {
       const auth = getAuth();
       return onAuthStateChanged(auth, (firebaseUser) => {
         if (firebaseUser) {
-          getUserData(firebaseUser.uid).then((user) => setUser(user));
+          getUserData(firebaseUser.uid)
+            .then((existingUser) => {
+              if (existingUser) {
+                setUser(existingUser);
+              } else {
+                registerNewUser(firebaseUser);
+              }
+            })
+            .catch(() => {
+              registerNewUser(firebaseUser);
+            });
         } else {
           setUser(null);
-          router.replace("/login"); // ログインされていない場合にリダイレクト
-          console.log("リダイレクトされました");
+          router.replace('/login');
+          console.log("リダイレクトしました。");
         }
       });
-    } catch {
+    } catch (error) {
       setUser(null);
-      router.replace("/login"); // エラー発生時にリダイレクト
-      console.log("エラーです");
+      router.replace('/login');
+      console.log("ログイン時にエラー出ました。");
     }
   }, [router]);
 
