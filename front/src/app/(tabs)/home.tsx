80582456424639895e6919b3a9308304bd33465d
GaiKOZAKI <125722953+GaiKOZAKI@users.noreply.github.com>
2024-06-16 15:55:47 +0900

    〇×ボタンで表示されるユーザーが変更されるようにした

--- front/src/app/(tabs)/home.tsx
+++ front/src/app/(tabs)/home.tsx
@@ -1,21 +1,60 @@
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, ImageBackground } from "react-native";
 
 import Button from "../../components/Button";
-
-const handlePress = (): void => {};
+import { API_ENDPOINT } from "../../env";
+import { useAuthContext } from "../../provider/AuthProvider";
+import type { User } from "../../types";
 
 const image = { uri: "https://legacy.reactjs.org/logo-og.png" };
 
 const Index = (): JSX.Element => {
+  const [users, setUsers] = useState<User[] | null>(null);
+  const [displayedUser, setDisplayedUser] = useState<User | null>(null);
+  const currentUserId = useAuthContext()?.id;
+
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const response = await fetch(`${API_ENDPOINT}/users/all`);
+        const data = await response.json();
+        if (!currentUserId) return;
+        const otherUsers = data.filter(
+          (user: User) => user.id !== currentUserId,
+        );
+        setUsers(otherUsers);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    getUsers();
+  }, [currentUserId]);
+
+  useEffect(() => {
+    if (users) {
+      const randomIndex = Math.floor(Math.random() * users.length);
+      setDisplayedUser(users[randomIndex]);
+    }
+  }, [users]);
+
+  const handlePress = (): void => {
+    if (!users || !displayedUser) return;
+    const newUsers = users?.filter((user) => user.id !== displayedUser?.id);
+    setUsers(newUsers);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageBackground source={image} style={styles.backGroundImage}>
           <View style={styles.nameTagContainer}>
-            <Text style={styles.nameTagLabel}>Michael Jordan</Text>
-            <Text style={styles.nameTagText}>
-              Profile Text Sample
-              あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。
+            <Text style={styles.nameTagLabel}>
+              Name:
+              {displayedUser?.name}
+            </Text>
+            <Text style={styles.nameTagLabel}>
+              id:
+              {displayedUser?.id}
             </Text>
           </View>
         </ImageBackground>
