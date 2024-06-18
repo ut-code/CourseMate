cbf7195df792afa6d9a3bdee827373745889bdec
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-06-19 00:51:14 +0900

    setup mui

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import "./index.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const router = createBrowserRouter([
   {
