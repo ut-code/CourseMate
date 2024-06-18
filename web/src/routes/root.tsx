fb5d6c29ebf08d0e6f308777cfcb3b3578af9fc9
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-06-19 02:20:45 +0900

    AppBarを追加

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -1,16 +1,38 @@
-import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import {
+  AppBar,
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import EmailIcon from "@mui/icons-material/Email";
 import PeopleIcon from "@mui/icons-material/People";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export default function Root() {
   const [value, setValue] = useState(0);
+  const labels = ["Home", "Profile", "Followers", "Requests"];
 
   return (
     <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {labels[value]}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <div>
         <Outlet />
       </div>
