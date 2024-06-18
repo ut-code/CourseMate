389cb3f5347319999b783c61b1646e3f75a8d449
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-06-19 02:41:00 +0900

    Profileページの大枠を作成

--- web/src/routes/tabs/profile.tsx
+++ web/src/routes/tabs/profile.tsx
@@ -1,7 +1,20 @@
+import { Box } from "@mui/material";
+import { User } from "../../../../common/types";
+
 export default function Profile() {
+  // TODO: Implement this
+  // const user = useAuthContext();
+  const SAMPLE_USER: User = {
+    id: 1,
+    uid: "hogehoge",
+    name: "ほげ",
+    email: "hoge@example.com",
+  };
+
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <Box>
+      <p>Name: {SAMPLE_USER.name}</p>
+      <p>ID: {SAMPLE_USER.id}</p>
+    </Box>
   );
 }
