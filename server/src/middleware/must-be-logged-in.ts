e3370d5d290182b335ba25b3de3f76929cddd891
aster <137767097+aster-void@users.noreply.github.com>
2024-07-15 14:08:58 +0900

    run prettier

--- server/src/middleware/must-be-logged-in.ts
+++ server/src/middleware/must-be-logged-in.ts
@@ -4,21 +4,27 @@ import mustSatisfy from "./must-satisfy";
 const maxAge = 5 * 1000; // its unit is ms in express.
 
 // requires cookieParser before this middleware.
-const mustBeLoggedIn = mustSatisfy((req: express.Request) => {
-  if (typeof req.cookies.id !== "string" || typeof req.cookies.session !== "string") {
-    return false
-  }
-  // TODO: use req.cookies.id and req.cookies.session to check if user is logged in
-  // (or just requiring existence of the cookies is also fine)
-  // probably requires some kind of DB conn.
-  // it probably won't be really efficient, so cache or make a JWT or smth
+const mustBeLoggedIn = mustSatisfy(
+  (req: express.Request) => {
+    if (
+      typeof req.cookies.id !== "string" ||
+      typeof req.cookies.session !== "string"
+    ) {
+      return false;
+    }
+    // TODO: use req.cookies.id and req.cookies.session to check if user is logged in
+    // (or just requiring existence of the cookies is also fine)
+    // probably requires some kind of DB conn.
+    // it probably won't be really efficient, so cache or make a JWT or smth
 
-  return true;
-}, function(res: express.Response) {
-  // TODO: res.redirect(302, "http://localhost:3000/path/to/login/page");
-  res.cookie("id", "you are", { maxAge });
-  res.cookie("session", "not logged in", { maxAge });
-  res.send("granted cookie");
-});
+    return true;
+  },
+  function (res: express.Response) {
+    // TODO: res.redirect(302, "http://localhost:3000/path/to/login/page");
+    res.cookie("id", "you are", { maxAge });
+    res.cookie("session", "not logged in", { maxAge });
+    res.send("granted cookie");
+  },
+);
 
 export default mustBeLoggedIn;
