196111a0a7f695c1bcbb89d76a6566912f552bdd
Yuki Kobayashi <137767097+aster-void@users.noreply.github.com>
2024-07-25 20:10:49 +0900

    fix cookie name (#66)

--- server/src/firebase/auth/lib.ts
+++ server/src/firebase/auth/lib.ts
@@ -8,7 +8,7 @@ const app = getAuth();
 // REQUIRE: cookieParser middleware before this
 // THROWS: if idToken is not present in request cookie, or when the token is not valid.
 export async function verify(req: Request): Promise<UID> {
-  const idToken = req.cookies.idToken;
+  const idToken = req.cookies["id-token"];
   if (!idToken)
     throw new Error(
       "idToken not found on request.cookies. you must set it to the cookies before sending the request.",
