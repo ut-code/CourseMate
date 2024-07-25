fb8c5a77732715b5a207dde8a4cd14adc3b8558a
aster <137767097+aster-void@users.noreply.github.com>
2024-07-25 18:53:54 +0900

    make function doWithIdToken(func)

--- web/src/firebase/auth/lib.ts
+++ web/src/firebase/auth/lib.ts
@@ -16,3 +16,18 @@ export async function refreshIdToken(): Promise<void> {
   const idToken = await getIdToken();
   await setIdTokenCookie(idToken);
 }
+
+/** 
+ * given func runs at most twice. do not mutate external variables inside the function.
+ * throws error when:
+ * - it failed to get id token
+ * - the func failed with id token
+ **/
+export async function doWithIdToken<T>(func: () => Promise<T>): Promise<T> {
+  try {
+    return await func();
+  } catch {
+    refreshIdToken();
+    return await func();
+  }
+}
