f41eba2cb48f1765cd1f27d5d74a143d2fac6ef8
Yuki Kobayashi <137767097+aster-void@users.noreply.github.com>
2024-07-25 17:16:58 +0900

    idToken が cookie にあれば認証が可能(現在はない) (#57)
    * init
    
    * fix all
    
    * run prettier

---
+++ server/src/firebase/auth/lib.ts
@@ -0,0 +1,18 @@
+import { getAuth } from "firebase-admin/auth";
+import type { Request } from "express";
+
+type UID = string;
+
+const app = getAuth();
+
+// REQUIRE: cookieParser middleware before this
+// THROWS: if idToken is not present in request cookie, or when the token is not valid.
+export async function verify(req: Request): Promise<UID> {
+  const idToken = req.cookies.idToken;
+  if (!idToken)
+    throw new Error(
+      "idToken not found on request.cookies. you must set it to the cookies before sending the request.",
+    );
+  const token = await app.verifyIdToken(idToken);
+  return token.uid;
+}
