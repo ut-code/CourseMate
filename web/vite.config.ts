3dbcca40b6ed9866f7b61304a0fb3ae13fc5781b
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-07-25 19:56:09 +0900

    integrate unplugin-typia into vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import UnpluginTypia from "@ryoppippi/unplugin-typia/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    UnpluginTypia({
+      /* options */
+    }),
+  ],
 });
