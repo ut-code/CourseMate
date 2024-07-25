8fe404c6f449794f44d0786796db5fae9e79d760
aster <137767097+aster-void@users.noreply.github.com>
2024-07-25 17:48:42 +0900

    add asyncMust

--- server/src/middleware/must-satisfy.ts
+++ server/src/middleware/must-satisfy.ts
@@ -1,10 +1,10 @@
 import type { Request, Response } from "express";
 
-function must(
+export function must(
   validate: (req: Request) => boolean,
-  onError: ((res: Response) => void) | string,
+  onError: ((res: Response) => void | Promise<void>) | string,
 ): (req: Request, res: Response, next: () => void) => void {
-  return function (req: Request, res: Response, next: () => void) {
+  return function(req: Request, res: Response, next: () => void) {
     if (!validate(req)) {
       if (typeof onError === "string") {
         res.send(onError);
@@ -13,9 +13,29 @@ function must(
       }
       return;
     }
-
     next();
   };
 }
 
-export default must;
+export function asyncMust(
+  validate: (req: Request) => Promise<boolean>,
+  onError: ((res: Response) => void | Promise<void>) | string,
+): (req: Request, res: Response, next: () => void) => void {
+  return function(req: Request, res: Response, next: () => void) {
+    validate(req).then((ok) => {
+      if (ok) next();
+      else throw new Error("reject");
+    }).catch(() => {
+      if (typeof onError === "string") {
+        res.send(onError);
+      } else {
+        onError(res);
+      }
+    });
+  }
+};
+
+export default {
+  must,
+  asyncMust,
+};
