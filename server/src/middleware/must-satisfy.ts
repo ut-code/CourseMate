95556e01aed1e60785a8b420df1256bcbf16a0de
aster <137767097+aster-void@users.noreply.github.com>
2024-07-25 17:54:47 +0900

    run prettier

--- server/src/middleware/must-satisfy.ts
+++ server/src/middleware/must-satisfy.ts
@@ -4,7 +4,7 @@ export function must(
   validate: (req: Request) => boolean,
   onError: ((res: Response) => void | Promise<void>) | string,
 ): (req: Request, res: Response, next: () => void) => void {
-  return function(req: Request, res: Response, next: () => void) {
+  return function (req: Request, res: Response, next: () => void) {
     if (!validate(req)) {
       if (typeof onError === "string") {
         res.send(onError);
@@ -21,19 +21,21 @@ export function asyncMust(
   validate: (req: Request) => Promise<boolean>,
   onError: ((res: Response) => void | Promise<void>) | string,
 ): (req: Request, res: Response, next: () => void) => void {
-  return function(req: Request, res: Response, next: () => void) {
-    validate(req).then((ok) => {
-      if (ok) next();
-      else throw new Error("reject");
-    }).catch(() => {
-      if (typeof onError === "string") {
-        res.send(onError);
-      } else {
-        onError(res);
-      }
-    });
-  }
-};
+  return function (req: Request, res: Response, next: () => void) {
+    validate(req)
+      .then((ok) => {
+        if (ok) next();
+        else throw new Error("reject");
+      })
+      .catch(() => {
+        if (typeof onError === "string") {
+          res.send(onError);
+        } else {
+          onError(res);
+        }
+      });
+  };
+}
 
 export default {
   must,
