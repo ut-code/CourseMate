524c7e4e448e57034f91aed18c97f800ff0da274
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-07-26 11:42:02 +0900

    delete UnpluginTypia

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import UnpluginTypia from "@ryoppippi/unplugin-typia/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
