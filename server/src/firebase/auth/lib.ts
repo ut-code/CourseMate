d793ef1bd4031b5f864ac6e054c2f0f9e7fb7bac
aster <137767097+aster-void@users.noreply.github.com>
2024-07-26 19:00:27 +0900

    I don't understand what happened but it works now so it works. https://github.com/firebase/firebase-admin-node/issues/593

--- server/src/firebase/auth/lib.ts
+++ server/src/firebase/auth/lib.ts
@@ -1,8 +1,10 @@
-import { DecodedIdToken, getAuth } from "firebase-admin/auth";
+import { app } from "../init"
+import * as admin from "firebase-admin/auth"
 import type { Request } from "express";
 import { GUID, IDToken } from "../../../../common/types";
 
-const app = getAuth();
+const auth = admin.getAuth(app);
+type DecodedIdToken = admin.DecodedIdToken;
 
 // REQUIRE: cookieParser middleware before this
 // THROWS: if idToken is not present in request cookie, or when the token is not valid.
@@ -16,5 +18,5 @@ export async function verifyIDToken(idToken: IDToken): Promise<DecodedIdToken> {
     throw new Error(
       "idToken not found on request.cookies. you must set it to the cookies before sending the request.",
     );
-  return await app.verifyIdToken(idToken);
+  return await auth.verifyIdToken(idToken);
 }
---
+++ server/src/firebase/init.ts
@@ -0,0 +1,8 @@
+import * as admin from "firebase-admin";
+import { Auth } from "firebase-admin/lib/auth/auth";
+
+const app = admin.initializeApp();
+
+export {
+  app,
+}
