d2a1fcc520a6cfe45c8458efadfe983868d47266
aster <137767097+aster-void@users.noreply.github.com>
2024-07-26 18:03:29 +0900

    [auto] prettier

--- common/lib/result/safeParseInt.ts
+++ common/lib/result/safeParseInt.ts
@@ -1,10 +1,8 @@
 import { Err, Ok, Result } from "../result";
 
 export function safeParseInt(s: string | undefined): Result<number> {
-  if (!s)
-    return Err(new Error("empty string"));
+  if (!s) return Err(new Error("empty string"));
   const n = parseInt(s);
-  if (Number.isNaN(n))
-    return Err(new Error("invalid formatting: "+s));
+  if (Number.isNaN(n)) return Err(new Error("invalid formatting: " + s));
   return Ok(n);
 }
