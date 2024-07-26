3677d4b6ce874649b5f97dc644d1aecab435ccfb
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-07-26 11:26:33 +0900

    delete @ryoppippi/unplugin-typia/vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,10 +4,5 @@ import UnpluginTypia from "@ryoppippi/unplugin-typia/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    UnpluginTypia({
-      /* options */
-    }),
-  ],
+  plugins: [react()],
 });
