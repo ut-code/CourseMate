8636d49f1ef2ce2da388d64fa9571e6946fab7d2
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-07-26 11:50:12 +0900

    introduce createAssert

--- common/typia.ts
+++ common/typia.ts
@@ -8,27 +8,10 @@ import {
   UserID,
 } from "./types";
 
-//引数がUser型に対して正しいかどうかをチェックする
-export function assertUser(input: unknown): User {
-  return typia.assert<User>(input);
-}
-
-export function assertRelationship(input: unknown): Relationship {
-  return typia.assert<Relationship>(input);
-}
-
-export function assertPublicUser(input: unknown): PublicUser {
-  return typia.assert<PublicUser>(input);
-}
-
-export function assertUserID(input: unknown): UserID {
-  return typia.assert<UserID>(input);
-}
-
-export function assertGUID(input: unknown): GUID {
-  return typia.assert<GUID>(input);
-}
-
-export function assertPartialUser(input: unknown): PartialUser {
-  return typia.assert<PartialUser>(input);
-}
+//引数が指定した型に対して正しいかどうかを検証する
+export const assertUser = typia.createAssert<User>();
+export const assertRelationship = typia.createAssert<Relationship>();
+export const assertPublicUser = typia.createAssert<PublicUser>();
+export const assertUserID = typia.createAssert<UserID>();
+export const assertGUID = typia.createAssert<GUID>();
+export const assertPartialUser = typia.createAssert<PartialUser>();
