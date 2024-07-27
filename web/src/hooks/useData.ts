015cd9130ef43ca3c44cdc30f7dd72027c6831e4
aster <137767097+aster-void@users.noreply.github.com>
2024-07-27 14:35:50 +0900

    make hook useAuthorizedError(url) and make api/hooks use this instead

--- web/src/hooks/useData.ts
+++ web/src/hooks/useData.ts
@@ -1,4 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
+import { ErrUnauthorized, refreshIdToken } from "../firebase/auth/lib";
+import { Ok, Err } from "../../../common/lib/result";
 
 // TODO: separate this into concrete types and urls s.t. there is no unsafe any
 // also use sth like Typia (or Zod if you really like it)
@@ -30,4 +32,45 @@ export default function useData<T>(url: string) {
   }, [reload]);
 
   return { data, isLoading, error, reload };
+}
+
+// TODO: refactor this to look better.
+export function useAuthorizedData<T>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const safeReadData = async () => {
+    try {
+      const response = await fetch(url);
+      if (response.status === 401) throw new ErrUnauthorized();
+      if (!response.ok) throw new Error("Response was not ok.");
+      const result = await response.json();
+      setData(result);
+      return Ok(null);
+    } catch (e) {
+      if (error instanceof Error) {
+        setError(error);
+        setData(null);
+      }
+      return Err(e);
+    }
+  };
+
+  const reload = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    const result = await safeReadData();
+    if (!result.ok && result.error instanceof ErrUnauthorized) {
+      await refreshIdToken();
+      await safeReadData();
+    }
+    setIsLoading(false);
+}, []);
+
+useEffect(() => {
+  reload();
+}, [reload]);
+
+return { data, isLoading, error, reload };
 }
