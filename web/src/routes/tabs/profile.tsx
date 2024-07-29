b5549bba9d66722fbedce7cdc68dd2213b670a33
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-07-29 13:32:02 +0900

    useMe Hookを導入して、レンダリングされない問題を解決

--- web/src/routes/tabs/profile.tsx
+++ web/src/routes/tabs/profile.tsx
@@ -1,15 +1,12 @@
 import { Box, Button } from "@mui/material";
 import LogOutButton from "../../components/LogOutButton";
-import { useEffect, useState } from "react";
-import { getAuth } from "firebase/auth";
-import { User } from "../../../../common/types";
+import { useState } from "react";
 import EditUserDialog from "../../components/EditUserDialog";
-import userapi from "../../api/user";
+import hooks from "../../api/hooks";
 
 export default function Profile() {
-  const [user, setUser] = useState<User | null>(null);
-
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const { data, isLoading, error } = hooks.useMe();
 
   const handleDialogOpen = () => {
     setIsDialogOpen(true);
@@ -18,41 +15,39 @@ export default function Profile() {
   const handleDialogClose = () => {
     setIsDialogOpen(false);
   };
-  useEffect(() => {
-    const user = getAuth().currentUser;
-    if (!user) return;
-    userapi
-      .aboutMe()
-      .then((data) => setUser(data))
-      .catch((error) => console.error("Error fetching user data:", error));
-  }, []);
 
-  if (user) {
-    return (
-      <Box>
-        <Box>
-          <p>Name: {user.name}</p>
-          <p>ID: {user.id}</p>
-          {user.pictureUrl && (
-            <img
-              src={user.pictureUrl}
-              alt="Profile Picture"
-              style={{ width: "300px", height: "300px", objectFit: "cover" }} // 画像のサイズを指定
-            />
-          )}
-        </Box>
-        <LogOutButton />
-        <Button color="inherit" onClick={handleDialogOpen}>
-          プロフィールを編集
-        </Button>
-        <EditUserDialog
-          userId={user.id}
-          open={isDialogOpen}
-          onClose={handleDialogClose}
-        />
-      </Box>
-    );
-  } else {
-    return <p>ユーザ情報が取得できませんでした。</p>;
-  }
+  return (
+    <Box>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : data ? (
+        <>
+          <Box>
+            <p>Name: {data.name}</p>
+            <p>ID: {data.id}</p>
+            {data.pictureUrl && (
+              <img
+                src={data.pictureUrl}
+                alt="Profile Picture"
+                style={{ width: "300px", height: "300px", objectFit: "cover" }}
+              />
+            )}
+          </Box>
+          <LogOutButton />
+          <Button color="inherit" onClick={handleDialogOpen}>
+            プロフィールを編集
+          </Button>
+          <EditUserDialog
+            userId={data.id}
+            open={isDialogOpen}
+            onClose={handleDialogClose}
+          />
+        </>
+      ) : (
+        <p>データがありません。</p>
+      )}
+    </Box>
+  );
 }
