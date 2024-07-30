62be85dbac7e4d08138a7d51b45b5832b2f64349
aster <137767097+aster-void@users.noreply.github.com>
2024-07-31 02:52:31 +0900

    move types to common

--- common/types.ts
+++ common/types.ts
@@ -1,11 +1,21 @@
-type RelationshipStatus = "PENDING" | "MATCHED" | "REJECTED";
+// GLOBAL
 
-export type Relationship = {
-  id: number;
-  sendingUserId: UserID;
-  receivingUserId: UserID;
-  status: RelationshipStatus;
-};
+/**
+ * Google account's id.
+ * recommended variable name: guid
+ **/
+export type GUID = string & { __internal_prevent_implicit_cast_GUID: PhantomData };
+
+/**
+ * User's ID used in database. do not assign string to this.
+ * recommended variable name: id
+ **/
+export type UserID = number & { __internal_prevent_implicit_cast_UserID: PhantomData };
+
+/**
+ * Firebase ID Token.
+ **/
+export type IDToken = string;
 
 export type User = {
   id: UserID;
@@ -23,6 +33,7 @@ export type PublicUser = {
   name: string;
   pictureUrl: string;
 };
+
 export function Public(u: User): PublicUser {
   return {
     id: u.id,
@@ -30,20 +41,80 @@ export function Public(u: User): PublicUser {
     pictureUrl: u.pictureUrl,
   };
 }
+// MATCHES
 
-/**
- * Google account's id.
- * recommended variable name: guid
- **/
-export type GUID = string;
+type RelationshipStatus = "PENDING" | "MATCHED" | "REJECTED";
 
-/**
- * User's ID used in database. do not assign string to this.
- * recommended variable name: id
- **/
-export type UserID = number;
+export type Relationship = {
+  id: number;
+  sendingUserId: UserID;
+  receivingUserId: UserID;
+  status: RelationshipStatus;
+};
 
-/**
- * Firebase ID Token.
- **/
-export type IDToken = string;
+/*
+# Chat Rooms
+
+- chat (domain)
+  - Room (= All chattable places)
+    - log
+  - DM : part of Room
+  - SharedRoom : also part of Room
+*/
+
+type PhantomData = never;
+
+export type MessageID = number & { __internal_prevent_cast_MessageID: PhantomData };
+export type ShareRoomID = number & { __internal_prevent_cast_RoomID: PhantomData };
+export type DMRoomID = number & { __internal_prevent_cast_DMID: PhantomData };
+export type Message = {
+  id: MessageID;
+  sender: UserID;
+  content: string;
+  createdAt: Date;
+};
+
+export type SendMessage = {
+  content: string;
+};
+
+// the data type to be displayed at initial screen (in list)
+export type RoomOverview = SharedRoomOverview | DMOverview;
+
+export type DMOverview = {
+  thumbnail: string;
+  lastmsg?: Message;
+  isDM: true;
+  dmid: DMRoomID;
+};
+
+export type SharedRoomOverview = {
+  thumbnail: string;
+  lastmsg?: Message;
+  isDM: false;
+  roomId: ShareRoomID;
+};
+
+export type DMRoom = {
+  id: DMRoomID; // TODO: should this use Relationship.ID or RoomID?
+  isDM: true;
+  member: User[];
+  messages: Message[];
+};
+
+export type SharedRoom = {
+  id: ShareRoomID;
+  name: string;
+  isDM: false;
+  member: User[];
+  messages: Message[];
+};
+
+export type InitRoom = Omit<Omit<SharedRoom, "id">, "log">;
+export type InitSharedRoom = InitRoom & { isDM: false };
+
+export type UpdateRoom = {
+  name: string;
+  pictureUrl: string;
+};
+
