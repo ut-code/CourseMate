7964f4b88f38228bf53f0c4dde087537182e5b2a
aster <137767097+aster-void@users.noreply.github.com>
2024-08-05 13:04:37 +0900

    rewrite it

---
+++ web/src/components/ChatStack.tsx
@@ -0,0 +1,78 @@
+import {
+  Button,
+  Stack,
+  Typography,
+  ListItem,
+  TextField,
+  IconButton,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import { DMOverview, RoomOverview, SendMessage } from "../common/types";
+import { useState } from "react";
+import * as chat from "../api/chat/chat";
+
+type Props = {
+  send: (ro: RoomOverview, m: SendMessage) => void;
+  room: DMOverview;
+};
+export function ChatStack(props: Props) {
+  const { room, send } = props;
+
+  const [message, setMessage] = useState<string>("");
+
+  return (
+    <ListItem
+      key={room.friendId}
+      sx={{
+        mb: 1,
+        border: "2px solid #1976D2",
+        borderRadius: 1,
+        padding: 5,
+      }}
+      secondaryAction={
+        <Stack
+          direction={"row"}
+          spacing={2}
+          alignItems="center"
+          textAlign={"center"}
+        >
+          <Typography variant="body2">{room.name}</Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              chat
+                .getDM(room.friendId)
+                .then((data) => alert(data.messages.join("\n")));
+            }}
+          >
+            チャットの内容を表示
+          </Button>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              const msg = "hi";
+              send(room, {
+                content: msg,
+              });
+            }}
+          >
+            <Stack direction="row" spacing={1} alignItems="center">
+              <TextField
+                name="message"
+                placeholder="メッセージ"
+                variant="outlined"
+                size="small"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <IconButton type="submit" color="primary">
+                <SendIcon />
+              </IconButton>
+            </Stack>
+          </form>
+        </Stack>
+      }
+    />
+  );
+}
--- web/src/routes/tabs/chat.tsx
+++ web/src/routes/tabs/chat.tsx
@@ -1,39 +1,18 @@
-import {
-  Box,
-  List,
-  ListItem,
-  Button,
-  Stack,
-  Typography,
-  TextField,
-  IconButton,
-} from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";
+import { Box, List, Typography } from "@mui/material";
 import * as chat from "../../api/chat/chat";
 import { useRoomsOverview } from "../../api/chat/hooks";
-import { useState } from "react";
-import { RoomOverview } from "../../common/types";
+import { RoomOverview, SendMessage } from "../../common/types";
+import { ChatStack } from "../../components/ChatStack";
 
 export default function Chat() {
   const { data, error, loading, reload } = useRoomsOverview();
-  const [messages, setMessages] = useState<{ [key: string]: string }>({});
 
-  const handleChange = (friendId: number, value: string) => {
-    setMessages((prevMessages) => ({
-      ...prevMessages,
-      [friendId]: value,
-    }));
-  };
-
-  const submitMessage = async (room: RoomOverview) => {
+  const submitMessage = async (room: RoomOverview, msg: SendMessage) => {
     if (room.isDM) {
-      const msg = { content: messages[room.friendId] || "" };
       await chat.sendDM(room.friendId, msg);
     } else {
-      const msg = { content: messages[room.roomId] || "" };
       await chat.send(room.roomId, msg);
     }
-    setMessages({});
     reload();
   };
 
@@ -48,64 +27,7 @@ export default function Chat() {
           {data !== undefined &&
             data?.map((room) => {
               if (room.isDM) {
-                return (
-                  <ListItem
-                    key={room.friendId}
-                    sx={{
-                      mb: 1,
-                      border: "2px solid #1976D2",
-                      borderRadius: 1,
-                      padding: 5,
-                    }}
-                    secondaryAction={
-                      <Stack
-                        direction={"row"}
-                        spacing={2}
-                        alignItems="center"
-                        textAlign={"center"}
-                      >
-                        <Typography variant="body2">{room.name}</Typography>
-                        <Button
-                          variant="contained"
-                          color="primary"
-                          onClick={() => {
-                            chat
-                              .getDM(room.friendId)
-                              .then((data) => alert(data.messages.join("\n")));
-                          }}
-                        >
-                          チャットの内容を表示
-                        </Button>
-                        <form
-                          onSubmit={(e) => {
-                            e.preventDefault();
-                            submitMessage(room);
-                          }}
-                        >
-                          <Stack
-                            direction="row"
-                            spacing={1}
-                            alignItems="center"
-                          >
-                            <TextField
-                              name="message"
-                              placeholder="メッセージ"
-                              variant="outlined"
-                              size="small"
-                              value={messages[room.friendId] || ""}
-                              onChange={(e) =>
-                                handleChange(room.friendId, e.target.value)
-                              }
-                            />
-                            <IconButton type="submit" color="primary">
-                              <SendIcon />
-                            </IconButton>
-                          </Stack>
-                        </form>
-                      </Stack>
-                    }
-                  />
-                );
+                return <ChatStack send={submitMessage} room={room} />;
               } else {
                 return (
                   <Typography key={room.roomId} variant="body2" sx={{ mb: 1 }}>
