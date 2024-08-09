b3db9f26293f88f78c6edbbaa5de469c574fa791
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-08-09 11:57:42 +0900

    add myReq components

---
+++ web/src/components/match/myRequests.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+export default function MyReq() {
+  return (
+    <Box>
+      <p>MyReq</p>
+    </Box>
+  );
+}
---
+++ web/src/components/match/othersRequests.tsx
@@ -0,0 +1,69 @@
+import {
+  Box,
+  List,
+  ListItem,
+  Button,
+  Stack,
+  ListItemAvatar,
+} from "@mui/material";
+import request from "../../api/request";
+import hooks from "../../api/hooks";
+import UserAvatar from "../avatar/avatar";
+
+export default function OthersReq() {
+  const { data, loading, error, reload } = hooks.usePendingUsers();
+
+  return (
+    <Box>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        <List>
+          {data !== undefined &&
+            // FIXME: this is probably not matched user
+            data?.map((matchedUser) => (
+              <ListItem
+                key={matchedUser.id.toString()}
+                secondaryAction={
+                  <Stack direction={"row"}>
+                    <Button
+                      onClick={() => {
+                        request.accept(matchedUser.id).then(() => reload());
+                      }}
+                    >
+                      承認
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        if (
+                          !window.confirm(
+                            "本当にこのマッチリクエストを拒否しますか?"
+                          )
+                        )
+                          return;
+                        request.reject(matchedUser.id).then(() => reload());
+                      }}
+                    >
+                      拒否
+                    </Button>
+                  </Stack>
+                }
+              >
+                <ListItemAvatar>
+                  <UserAvatar
+                    pictureUrl={matchedUser.pictureUrl}
+                    width="50px"
+                    height="50px"
+                  />
+                </ListItemAvatar>
+
+                <p>{matchedUser.name}</p>
+              </ListItem>
+            ))}
+        </List>
+      )}
+    </Box>
+  );
+}
