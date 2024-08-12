01863a61a1eee7e55d8f57363163607c9b4495cc
aster <137767097+aster-void@users.noreply.github.com>
2024-08-13 02:52:13 +0900

    revert change on upload-photo.ts

--- web/src/firebase/store/upload-photo.ts
+++ web/src/firebase/store/upload-photo.ts
@@ -21,23 +21,3 @@ export async function uploadImage(pictureFile: File): Promise<string> {
     throw new Error("画像のアップロードに失敗しました");
   }
 }
-
-// ImageData 形式の画像をfirestoreにアップロードする関数
-export async function uploadImageData(image: ImageData): Promise<string> {
-  const guid = getAuth().currentUser?.uid;
-  if (!guid) throw new Error("Failed to get user");
-
-  const storage = getStorage(app);
-  const filePath = `${guid}/${name}`;
-  const storageRef = ref(storage, filePath);
-
-  try {
-    const snapshot = await uploadBytes(storageRef, image.data);
-    const pictureUrl = await getDownloadURL(snapshot.ref);
-    console.log("File available at", pictureUrl);
-    return pictureUrl;
-  } catch (error) {
-    console.error("Error uploading file:", error);
-    throw new Error("画像のアップロードに失敗しました");
-  }
-}
