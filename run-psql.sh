a4918dfb552961d26654bbbbc95fce17d6b58e51
Yuki Kobayashi <137767097+aster-void@users.noreply.github.com>
2024-08-14 02:08:23 +0900

    no detach

--- run-psql.sh
+++ run-psql.sh
@@ -3,7 +3,7 @@
 # Launch postgres server at localhost.
 # to use this, change server/.env:DATABASE_URL to
 # DATABASE_URL=postgres://user:password@localhost:5432/database
-docker run -d --rm --name postgres -p 5432:5432 \
+docker run --rm --name postgres -p 5432:5432 \
   -e POSTGRES_PASSWORD=password \
   -e POSTGRES_USER=user \
   -e POSTGRES_DB=database \
