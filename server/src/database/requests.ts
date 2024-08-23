7be2b7ac8987e0e73bdf165bfac5f6a068619280
Yuki Kobayashi <137767097+aster-void@users.noreply.github.com>
2024-08-23 16:38:50 +0900

    server が database が落ちた時に落ちないようにする (#143)
    * make server/chat safe
    
    * work done
    
    * return Result<bool> instead of bool

--- server/src/database/requests.ts
+++ server/src/database/requests.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { User, UserID, Relationship } from "../common/types";
+import { Err, Ok, Result } from "../common/lib/result";
 
 const prisma = new PrismaClient();
 
@@ -10,157 +11,158 @@ export async function sendRequest({
 }: {
   senderId: UserID;
   receiverId: UserID;
-}): Promise<Relationship> {
+}): Promise<Result<Relationship>> {
   // 既存の関係をチェック
-  // TODO: fix this findFirst to be findUnique
-  const existingRelationship = await prisma.relationship.findFirst({
-    where: {
-      OR: [
-        { sendingUserId: senderId, receivingUserId: receiverId },
-        { sendingUserId: receiverId, receivingUserId: senderId }, // 逆の関係もチェック
-      ],
-    },
-  });
-  // 既存の関係がある場合はそのまま返す
-  if (existingRelationship) {
-    // 相手がすでにこちらに Request を送っている場合は approve (accept) したとみなす
-    if (existingRelationship.receivingUserId === senderId)
-      approveRequest(
-        existingRelationship.sendingUserId,
-        existingRelationship.receivingUserId,
-      );
-    return existingRelationship;
+  try {
+    // TODO: fix this findFirst to be findUnique
+    const existingRelationship = await prisma.relationship.findFirst({
+      where: {
+        OR: [
+          { sendingUserId: senderId, receivingUserId: receiverId },
+          { sendingUserId: receiverId, receivingUserId: senderId }, // 逆の関係もチェック
+        ],
+      },
+    });
+    // 既存の関係がある場合はそのまま返す
+    if (existingRelationship) {
+      // 相手がすでにこちらに Request を送っている場合は approve (accept) したとみなす
+      if (existingRelationship.receivingUserId === senderId)
+        approveRequest(
+          existingRelationship.sendingUserId,
+          existingRelationship.receivingUserId,
+        );
+      return Ok(existingRelationship);
+    }
+    const newRelationship = await prisma.relationship.create({
+      data: {
+        sendingUser: { connect: { id: senderId } },
+        receivingUser: { connect: { id: receiverId } },
+        status: "PENDING",
+      },
+    });
+    return Ok(newRelationship);
+  } catch (e) {
+    return Err(e);
   }
-  // 既存の関係がない場合は新しい関係を作成
-  const newRelationship = await prisma.relationship.create({
-    data: {
-      sendingUser: { connect: { id: senderId } },
-      receivingUser: { connect: { id: receiverId } },
-      status: "PENDING",
-    },
-  });
-  return newRelationship;
-}
-
-// 特定のユーザが送信・受信したマッチリクエストの取得
-// TODO: make is less generic s.t. it is safer to use and easier to maintain
-export async function getRequestsByUserId({
-  senderId,
-  receiverId,
-}: {
-  senderId?: UserID;
-  receiverId?: UserID;
-}): Promise<Relationship[]> {
-  if (senderId === undefined && receiverId === undefined) {
-    throw new Error("Either senderId or receiverId must be provided");
-  }
-  const whereClause: { sendingUserId?: UserID; receivingUserId?: UserID } = {};
-  if (senderId !== undefined) {
-    whereClause.sendingUserId = senderId;
-  }
-  if (receiverId !== undefined) {
-    whereClause.receivingUserId = receiverId;
-  }
-  const found = await prisma.relationship.findMany({
-    where: whereClause,
-  });
-  return found;
 }
 
 // マッチリクエストの承認
 export async function approveRequest(
   senderId: UserID,
   receiverId: UserID,
-): Promise<Relationship> {
-  const updated = await prisma.relationship.update({
-    where: {
-      sendingUserId_receivingUserId: {
-        sendingUserId: senderId,
-        receivingUserId: receiverId,
+): Promise<Result<Relationship>> {
+  try {
+    const updated = await prisma.relationship.update({
+      where: {
+        sendingUserId_receivingUserId: {
+          sendingUserId: senderId,
+          receivingUserId: receiverId,
+        },
       },
-    },
-    data: {
-      status: "MATCHED",
-    },
-  });
-  return updated;
+      data: {
+        status: "MATCHED",
+      },
+    });
+    return updated === null ? Err(404) : Ok(updated);
+  } catch (e) {
+    return Err(e);
+  }
 }
 
 // マッチリクエストの拒否
 export async function rejectRequest(
   senderId: UserID,
   receiverId: UserID,
-): Promise<Relationship> {
-  return await prisma.relationship.update({
-    where: {
-      sendingUserId_receivingUserId: {
-        sendingUserId: senderId,
-        receivingUserId: receiverId,
+): Promise<Result<Relationship>> {
+  try {
+    const rel = await prisma.relationship.update({
+      where: {
+        sendingUserId_receivingUserId: {
+          sendingUserId: senderId,
+          receivingUserId: receiverId,
+        },
       },
-    },
-    data: {
-      status: "REJECTED",
-    },
-  });
+      data: {
+        status: "REJECTED",
+      },
+    });
+    return rel === null ? Err(404) : Ok(rel);
+  } catch (e) {
+    return Err(e);
+  }
 }
 
 //ユーザーへのリクエストを探す 俺をリクエストしているのは誰だ
 export async function findPendingRequestsToUser(
   userId: UserID,
-): Promise<User[]> {
-  const found = await prisma.user.findMany({
-    where: {
-      sendingUsers: {
-        some: {
-          receivingUserId: userId,
-          status: "PENDING",
+): Promise<Result<User[]>> {
+  try {
+    const found = await prisma.user.findMany({
+      where: {
+        sendingUsers: {
+          some: {
+            receivingUserId: userId,
+            status: "PENDING",
+          },
         },
       },
-    },
-  });
-  return found;
+    });
+    return Ok(found);
+  } catch (e) {
+    return Err(e);
+  }
 }
 
 //ユーザーがリクエストしている人を探す 俺がリクエストしているのは誰だ
 export async function findPendingRequestsFromUser(
   userId: UserID,
-): Promise<User[]> {
-  const found = await prisma.user.findMany({
-    where: {
-      receivingUsers: {
-        some: {
-          sendingUserId: userId,
-          status: "PENDING",
+): Promise<Result<User[]>> {
+  try {
+    const found = await prisma.user.findMany({
+      where: {
+        receivingUsers: {
+          some: {
+            sendingUserId: userId,
+            status: "PENDING",
+          },
         },
       },
-    },
-  });
-  return found;
+    });
+    return Ok(found);
+  } catch (e) {
+    return Err(e);
+  }
 }
 
 //マッチした人の取得
-export async function searchMatchedUser(userId: UserID): Promise<User[]> {
-  const found = await prisma.user.findMany({
-    where: {
-      OR: [
-        {
-          sendingUsers: {
-            some: {
-              receivingUserId: userId,
-              status: "MATCHED",
+export async function searchMatchedUser(
+  userId: UserID,
+): Promise<Result<User[]>> {
+  try {
+    const found = await prisma.user.findMany({
+      where: {
+        OR: [
+          {
+            sendingUsers: {
+              some: {
+                receivingUserId: userId,
+                status: "MATCHED",
+              },
             },
           },
-        },
-        {
-          receivingUsers: {
-            some: {
-              sendingUserId: userId,
-              status: "MATCHED",
+          {
+            receivingUsers: {
+              some: {
+                sendingUserId: userId,
+                status: "MATCHED",
+              },
             },
           },
-        },
-      ],
-    },
-  });
-  return found;
+        ],
+      },
+    });
+    return Ok(found);
+  } catch (e) {
+    return Err(e);
+  }
 }
