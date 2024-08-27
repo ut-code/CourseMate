7d1a39e1f63f6f82b5845bd1470c460d8099b481
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-08-28 07:09:37 +0900

    delete comment

--- common/zod/methods.ts
+++ common/zod/methods.ts
@@ -25,122 +25,98 @@ import {
   UpdateRoomSchema,
 } from "./schemas";
 
-// Function to parse UserID
-export const parseUserID = (data: unknown) => {
+export function parseUserID(data: unknown) {
   return UserIDSchema.parse(data);
-};
+}
 
-// Function to parse GUID
-export const parseGUID = (data: unknown) => {
+export function parseGUID(data: unknown) {
   return GUIDSchema.parse(data);
-};
+}
 
-// Function to parse ID Token
-export const parseIDToken = (data: unknown) => {
+export function parseIDToken(data: unknown) {
   return IDTokenSchema.parse(data);
-};
+}
 
-// Function to parse Gender
-export const parseGender = (data: unknown) => {
+export function parseGender(data: unknown) {
   return GenderSchema.parse(data);
-};
+}
 
-// Function to parse Relationship Status
-export const parseRelationshipStatus = (data: unknown) => {
+export function parseRelationshipStatus(data: unknown) {
   return RelationshipStatusSchema.parse(data);
-};
+}
 
-// Function to parse User
-export const parseUser = (data: unknown) => {
+export function parseUser(data: unknown) {
   return UserSchema.parse(data);
-};
+}
 
-// Function to parse InitUser
-export const parseInitUser = (data: unknown) => {
+export function parseInitUser(data: unknown) {
   return InitUserSchema.parse(data);
-};
+}
 
-// Function to parse UpdateUser
-export const parseUpdateUser = (data: unknown) => {
+export function parseUpdateUser(data: unknown) {
   return UpdateUserSchema.parse(data);
-};
+}
 
-// Function to parse PublicUser
-export const parsePublicUser = (data: unknown) => {
+export function parsePublicUser(data: unknown) {
   return PublicUserSchema.parse(data);
-};
+}
 
-// Function to parse Relationship ID
-export const parseRelationshipID = (data: unknown) => {
+export function parseRelationshipID(data: unknown) {
   return RelationshipIDSchema.parse(data);
-};
+}
 
-// Function to parse Relationship
-export const parseRelationship = (data: unknown) => {
+export function parseRelationship(data: unknown) {
   return RelationshipSchema.parse(data);
-};
+}
 
-// Function to parse Message ID
-export const parseMessageID = (data: unknown) => {
+export function parseMessageID(data: unknown) {
   return MessageIDSchema.parse(data);
-};
+}
 
-// Function to parse ShareRoom ID
-export const parseShareRoomID = (data: unknown) => {
+export function parseShareRoomID(data: unknown) {
   return ShareRoomIDSchema.parse(data);
-};
+}
 
-// Function to parse Message
-export const parseMessage = (data: unknown) => {
+export function parseMessage(data: unknown) {
   return MessageSchema.parse(data);
-};
+}
 
-// Function to parse SendMessage
-export const parseSendMessage = (data: unknown) => {
+export function parseSendMessage(data: unknown) {
   return SendMessageSchema.parse(data);
-};
+}
 
-// Function to parse DMOverview
-export const parseDMOverview = (data: unknown) => {
+export function parseDMOverview(data: unknown) {
   return DMOverviewSchema.parse(data);
-};
+}
 
-// Function to parse SharedRoomOverview
-export const parseSharedRoomOverview = (data: unknown) => {
+export function parseSharedRoomOverview(data: unknown) {
   return SharedRoomOverviewSchema.parse(data);
-};
+}
 
-// Function to parse RoomOverview
-export const parseRoomOverview = (data: unknown) => {
+export function parseRoomOverview(data: unknown) {
   return RoomOverviewSchema.parse(data);
-};
+}
 
-// Function to parse DMRoom
-export const parseDMRoom = (data: unknown) => {
+export function parseDMRoom(data: unknown) {
   return DMRoomSchema.parse(data);
-};
+}
 
-// Function to parse PersonalizedDMRoom
-export const parsePersonalizedDMRoom = (data: unknown) => {
+export function parsePersonalizedDMRoom(data: unknown) {
   return PersonalizedDMRoomSchema.parse(data);
-};
+}
 
-// Function to parse SharedRoom
-export const parseSharedRoom = (data: unknown) => {
+export function parseSharedRoom(data: unknown) {
   return SharedRoomSchema.parse(data);
-};
+}
 
-// Function to parse InitRoom
-export const parseInitRoom = (data: unknown) => {
+export function parseInitRoom(data: unknown) {
   return InitRoomSchema.parse(data);
-};
+}
 
-// Function to parse InitSharedRoom
-export const parseInitSharedRoom = (data: unknown) => {
+export function parseInitSharedRoom(data: unknown) {
   return InitSharedRoomSchema.parse(data);
-};
+}
 
-// Function to parse UpdateRoom
-export const parseUpdateRoom = (data: unknown) => {
+export function parseUpdateRoom(data: unknown) {
   return UpdateRoomSchema.parse(data);
-};
+}
