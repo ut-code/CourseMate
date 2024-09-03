1b0aeee94dc7db1ff600278f01071c6128cff3f4
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-09-03 17:20:41 +0900

    setした

--- server/src/lib/socket/socket.ts
+++ server/src/lib/socket/socket.ts
@@ -3,7 +3,7 @@ import { corsOptions, server } from "../..";
 import { Message, UserID } from "../../common/types";
 import { getUserIdfromToken } from "../../firebase/auth/db";
 
-const users: { [key: string]: Socket } = {};
+const users = new Map<UserID, Socket>();
 
 export function initializeSocket() {
   const io = new Server(server, {
@@ -12,18 +12,22 @@ export function initializeSocket() {
   });
 
   io.on("connection", (socket) => {
-    console.log("a user connected");
+    console.log("A user connected");
 
     socket.on("register", async (token) => {
       const userId = await getUserIdfromToken(token);
-      users[userId] = socket;
-      console.log(`User registered: ${userId}`);
+      if (userId) {
+        users.set(userId, socket);
+        console.log(`User registered: ${userId}`);
+      } else {
+        console.log("Invalid token or failed to retrieve user ID");
+      }
     });
 
     socket.on("disconnect", () => {
-      for (const [id, socket2] of Object.entries(users)) {
+      for (const [id, socket2] of users.entries()) {
         if (socket2.id === socket.id) {
-          delete users[id];
+          users.delete(id);
           console.log(`User ${id} disconnected`);
           break;
         }
@@ -33,7 +37,7 @@ export function initializeSocket() {
 }
 
 export function sendMessage(message: Message, friendId: UserID) {
-  const socket = users[friendId];
+  const socket = users.get(friendId);
   if (socket) {
     socket.emit("newMessage", message);
     console.log(`Message sent to ${friendId}: ${message}`);
