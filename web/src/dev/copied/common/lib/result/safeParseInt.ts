ed6b6fbdcf049c43ac099aca42256795aac6e0f9
aster <137767097+aster-void@users.noreply.github.com>
2024-09-12 15:12:01 +0900

    delete unused file

--- web/src/dev/copied/common/lib/result/safeParseInt.ts
+++
@@ -1,8 +0,0 @@
-import { Err, Ok, Result } from "../result";
-
-export function safeParseInt(s: string | undefined): Result<number> {
-  if (!s) return Err(new Error("empty string"));
-  const n = parseInt(s);
-  if (Number.isNaN(n)) return Err(new Error("invalid formatting: " + s));
-  return Ok(n);
-}
