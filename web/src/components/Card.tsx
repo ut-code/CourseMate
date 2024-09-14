56c189a3f437677baea767f9fbc370b57aadc9fc
GaiKOZAKI <125722953+GaiKOZAKI@users.noreply.github.com>
2024-09-14 13:20:21 +0900

    カードの表裏表示と反転のUIを追加

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -1,38 +1,135 @@
 import { User } from "../common/types";
 import UserAvatar from "./avatar/avatar";
-import { Button } from "@mui/material";
-import React from "react";
-import { ProfileModal } from "./avatar/profileModal";
+import { useState } from "react";
+import CoursesTable from "./course/CoursesTable";
+import ThreeSixtyIcon from "@mui/icons-material/ThreeSixty";
 
 interface CardProps {
   displayedUser: User | null;
 }
 
 export const Card = ({ displayedUser }: CardProps) => {
-  const [modalOpen, setModalOpen] = React.useState(false);
-  const [selectedUser, setSelectedUser] = React.useState<User | null>(null);
-  const handleOpen = (selectedUser: User | null) => {
-    setModalOpen(true);
-    setSelectedUser(selectedUser);
+  const [isRotated, setIsRotated] = useState(false);
+
+  const handleRotate = () => {
+    setIsRotated(!isRotated);
   };
-  const handleClose = () => setModalOpen(false);
 
   return (
-    <div>
-      <p>Name: {displayedUser?.name}</p>
-      <p>id: {displayedUser?.id}</p>
-      <Button onClick={() => handleOpen(displayedUser)}>
-        <UserAvatar
-          pictureUrl={displayedUser?.pictureUrl}
-          width="300px"
-          height="300px"
+    <div
+      style={{
+        perspective: "1000px",
+        width: "70vw",
+        height: "70vh",
+        position: "relative",
+      }}
+      onClick={handleRotate}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          transformStyle: "preserve-3d",
+          transition: "transform 1s",
+          transform: isRotated ? "rotateY(180deg)" : "rotateY(0deg)",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            backfaceVisibility: "hidden",
+          }}
+        >
+          <CardFront displayedUser={displayedUser} />
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            backfaceVisibility: "hidden",
+            transform: "rotateY(180deg)",
+          }}
+        >
+          <CardBack displayedUser={displayedUser} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CardFront = ({ displayedUser }: CardProps) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#F7FCFF",
+        border: "2px solid #3596C6",
+        padding: "10px",
+        height: "100%",
+        overflow: "hidden",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <div style={{ width: "50%", maxWidth: "300px", maxHeight: "300px" }}>
+          <UserAvatar
+            pictureUrl={displayedUser?.pictureUrl}
+            width="80%"
+            height="auto"
+          />
+        </div>
+        <p style={{ fontSize: "4vw", fontWeight: "bold" }}>
+          {displayedUser?.name}
+        </p>
+      </div>
+      <div style={{ padding: "10px" }}>
+        <p>学年: 学部num年</p>
+        {displayedUser?.grade && <p>学年: {displayedUser.grade}</p>}
+        <p>所属： hoge学部hugahuga学科</p>
+        {displayedUser?.intro_short && (
+          <p>自己紹介: {displayedUser.intro_short}</p>
+        )}
+      </div>
+      <div style={{ position: "absolute", bottom: "0", right: "0", left: "0" }}>
+        <ThreeSixtyIcon
+          style={{ fontSize: "7vw", display: "block", margin: "auto" }}
         />
-      </Button>
-      <ProfileModal
-        selectedUser={selectedUser}
-        open={modalOpen}
-        handleClose={handleClose}
-      />
+      </div>
+    </div>
+  );
+};
+
+const CardBack = ({ displayedUser }: CardProps) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#F7FCFF",
+        border: "2px solid #3596C6",
+        padding: "10px",
+        height: "100%",
+        overflow: "hidden",
+      }}
+    >
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <p style={{ fontSize: "2vw", fontWeight: "bold" }}>
+          {displayedUser?.name}
+        </p>
+      </div>
+      <CoursesTable />
+      <div style={{ position: "absolute", bottom: "0", right: "0", left: "0" }}>
+        <ThreeSixtyIcon
+          style={{ fontSize: "7vw", display: "block", margin: "auto" }}
+        />
+      </div>
     </div>
   );
 };
