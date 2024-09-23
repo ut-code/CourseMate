d01b57cd72e2ae62ebc26f0657e32fb9f3558313
Ryoma <119156952+RRRyoma@users.noreply.github.com>
2024-09-23 17:30:42 +0900

    授業編集機能

--- web/src/routes/editCourses.tsx
+++ web/src/routes/editCourses.tsx
@@ -1,9 +1,11 @@
 import { Box, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import hooks from "../api/hooks";
+import CoursesTable from "../components/course/CoursesTable";
 
 export default function EditCourses() {
   const navigate = useNavigate();
-  // const { data, loading, error } = hooks.useMe();
+  const { data, loading, error } = hooks.useMe();
 
   const handleSave = () => {
     navigate("/settings"); // router.pushからnavigateに変更
@@ -16,38 +18,18 @@ export default function EditCourses() {
   return (
     <Box sx={{ padding: "20px", maxWidth: "350px", margin: "0 auto" }}>
       <h1>授業選択</h1>
-      <table
-        style={{
-          width: "100%",
-          borderCollapse: "collapse",
-          textAlign: "center",
-        }}
-      >
-        <thead>
-          <tr>
-            <th>月</th>
-            <th>火</th>
-            <th>水</th>
-            <th>木</th>
-            <th>金</th>
-            <th>土</th>
-          </tr>
-        </thead>
-        <tbody>
-          {[1, 2, 3, 4, 5, 6].map((row) => (
-            <tr key={row}>
-              {["月", "火", "水", "木", "金", "土"].map((day) => (
-                <td
-                  key={day}
-                  style={{ border: "1px solid #ddd", height: "50px" }}
-                >
-                  {day === "火" && row === 3 ? "英語" : ""}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : data ? (
+        <>
+          <CoursesTable userId={data.id} editable={true} />
+        </>
+      ) : (
+        <p>データがありません。</p>
+      )}
+
       <Box
         sx={{
           display: "flex",
