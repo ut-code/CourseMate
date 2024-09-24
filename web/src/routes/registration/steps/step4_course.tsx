935ae333a91ae0301a577a0ee8b49863be0659ff
GaiKOZAKI <125722953+GaiKOZAKI@users.noreply.github.com>
2024-09-24 09:29:07 +0900

    Fix registration (#238)
    * css needs some huge rewrite
    
    * leave comment s.t. others can fix this in the future
    
    * remove unused prop from NextButton: caller
    
    * cropのモーダルのスタイルを修正
    
    * 初期登録画面の処理を修正
    
    * stepsのファイル名を修正
    
    * 表示テキスト等の修正
    
    ---------
    
    Co-authored-by: aster <137767097+aster-void@users.noreply.github.com>
    Co-authored-by: KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>

---
+++ web/src/routes/registration/steps/step4_course.tsx
@@ -0,0 +1,35 @@
+import { CircularProgress } from "@mui/material";
+import { Box } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+import hooks from "../../../api/hooks";
+import CoursesTable from "../../../components/course/CoursesTable";
+import { NextButton } from "../common";
+
+export default function Step4() {
+  const navigate = useNavigate();
+  const { data, loading, error } = hooks.useMe();
+  return (
+    <Box>
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : data ? (
+        <Box
+          mt={2}
+          mx={2}
+          display="flex"
+          flexDirection="column"
+          gap={2}
+          alignItems="center"
+        >
+          <p>授業情報を登録しましょう</p>
+          <CoursesTable userId={data.id} editable={true} />
+          <NextButton onClick={() => navigate("/home")}>完了</NextButton>
+        </Box>
+      ) : (
+        <p>データがありません。</p>
+      )}
+    </Box>
+  );
+}
