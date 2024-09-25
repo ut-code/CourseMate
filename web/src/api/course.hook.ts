5a15afe1d9d24631e4ec914da17cb4f5d266990a
aster <137767097+aster-void@users.noreply.github.com>
2024-09-25 22:21:06 +0900

    fix type error

--- web/src/api/course.hook.ts
+++ web/src/api/course.hook.ts
@@ -1,7 +1,14 @@
+import { z } from "zod";
 import type { Course } from "../common/types";
+import { CourseSchema } from "../common/zod/schemas";
 import { type Hook, useSWR } from "../hooks/useSWR";
 import { getMyCourses } from "./course";
 
+const CourseListSchema = z.array(CourseSchema);
 export function useMyCourses(): Hook<Course[]> {
-  return useSWR("COURSEMATE_CACHE__useMyCourse()", () => getMyCourses());
+  return useSWR(
+    "COURSEMATE_CACHE__useMyCourse()",
+    () => getMyCourses(),
+    CourseListSchema,
+  );
 }
