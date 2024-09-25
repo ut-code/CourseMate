c0587a1b89cd7eae6c64fb8bbb829970c2b28c97
aster <137767097+aster-void@users.noreply.github.com>
2024-09-26 00:26:56 +0900

    install sharp

---
+++ server/src/functions/img/compress.ts
@@ -0,0 +1,7 @@
+import sharp from "sharp";
+
+const IMAGE_SIZE_PX = 320;
+
+export async function compressImage(buf: Buffer): Promise<Buffer> {
+  return await sharp(buf).resize(IMAGE_SIZE_PX).webp().toBuffer();
+}
