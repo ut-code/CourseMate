0cfdfdc8f52f856d03ed9303a600ba809261eee0
aster <137767097+aster-void@users.noreply.github.com>
2024-09-28 17:22:50 +0900

    update prisma version to latest (with a repo with 20 stars (now 21))

--- flake.nix
+++ flake.nix
@@ -3,30 +3,33 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
-    nixos-prisma.url = "github:pimeys/nixos-prisma";
+    prisma-utils.url = "github:VanCoding/nix-prisma-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, nixos-prisma, ... }:
+  outputs = { nixpkgs, flake-utils, prisma-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; overlays = [ nixos-prisma.overlay ]; };
+        pkgs = import nixpkgs { inherit system; };
+
+        prisma = (prisma-utils.lib.prisma-factory {
+          nixpkgs = pkgs;
+          prisma-fmt-hash = "sha256-atD5GZfmeU86mF1V6flAshxg4fFR2ews7EwaJWZZzbc="; # just copy these hashes for now, and then change them when nix complains about the mismatch
+          query-engine-hash = "sha256-8FTZaKmQCf9lrDQvkF5yWPeZ7TSVfFjTbjdbWWEHgq4=";
+          libquery-engine-hash = "sha256-USIdaum87ekGY6F6DaL/tKH0BAZvHBDK7zjmCLo//kM=";
+          schema-engine-hash = "sha256-k5MkxXViEqojbkkcW/4iBFNdfhb9PlMEF1M2dyhfOok=";
+        }).fromNpmLock
+          ./server/package-lock.json; # <--- path to our package-lock.json file that contains the version of prisma-engines
       in
       {
         devShell = pkgs.mkShell {
+          src = ./.;
           nativeBuildInputs = with pkgs; [ bashInteractive ];
           buildInputs = with pkgs; [
-            nodePackages.prisma
             gnumake
             bun
             biome
           ];
-          shellHook = with pkgs; ''
-            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${stdenv.cc.cc.lib}/lib"
-            export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
-            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
-            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
-          '';
+          shellHook = prisma.shellHook;
         };
       });
 }
