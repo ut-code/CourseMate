9b297f71e4e50c8fc3ba9fa55b2c5ad4d0c3a614
aster <137767097+aster-void@users.noreply.github.com>
2024-10-02 18:52:27 +0900

    compile

--- server/src/functions/engines/recommendation.test.ts
+++ server/src/functions/engines/recommendation.test.ts
@@ -8,11 +8,14 @@ beforeAll(() => {
 
 test("recommendation engine", async () => {
   const usersFor101 = await recommendedTo(101, 5, 0);
-  expect(usersFor101).toEqual([102, 103]);
+  if (!usersFor101.ok) throw new Error();
+  expect(usersFor101.value.map((entry) => entry.u.id)).toEqual([102, 103]);
 
   const usersFor102 = await recommendedTo(102, 5, 0);
-  expect(usersFor102).toEqual([103, 101]);
+  if (!usersFor102.ok) throw new Error();
+  expect(usersFor102.value.map((entry) => entry.u.id)).toEqual([103, 101]);
 
   const usersFor103 = await recommendedTo(103, 5, 0);
-  expect(usersFor103).toEqual([102, 101]);
+  if (!usersFor103.ok) throw new Error();
+  expect(usersFor103.value.map((entry) => entry.u.id)).toEqual([102, 101]);
 });
