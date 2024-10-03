5bcc7515090fbc98cdc6c3bc8896d8dcc7969347
Yuki Kobayashi <137767097+aster-void@users.noreply.github.com>
2024-10-03 02:56:44 -0700

    授業のオーバーラップの COUNT (#331)
    # PRの概要
    oneof #100
    
    ## 具体的な変更内容
    被っている授業が多い順でユーザーを取得する SQL の作成と Prisma のバグ解消。
    
    ## 影響範囲
    まだ web には反映していないため、なし。
    Prisma をアップデートするため flake をアップデートした。 (結局 5.19 以上のは入らなかったので戻しても良いが、 Bun
    とかのバージョンも上がってるし良いかという感じ。)
    
    ## 動作要件
    Prisma >=v5.19 が必要なため、パッケージのアップデートが必要。
    
    ## 補足
    <!-- レビューをする際に見てほしい点、ローカル環境で試す際の注意点、など -->
    
    ## レビューリクエストを出す前にチェック！
    
    - [x] 改めてセルフレビューしたか
    - [x] 手動での動作検証を行ったか
    - [x] server の機能追加ならば、テストを書いたか
      - 理由: 書いた
    - [x] 間違った使い方が存在するならば、それのドキュメントをコメントで書いたか
      - 理由: 間違った使い方は存在しない
    - [x] わかりやすいPRになっているか
    
    <!-- レビューリクエスト後は、Slackでもメンションしてお願いすることを推奨します。 -->

---
+++ server/prisma/sql/recommend.sql
@@ -0,0 +1,7 @@
+SELECT recv.id AS recv, COUNT(recv_enroll) AS overlap FROM "User" recv
+INNER JOIN "Enrollment" recv_enroll ON recv_enroll."userId" = recv.id
+INNER JOIN "Course" course ON recv_enroll."courseId" = course.id
+INNER JOIN "Enrollment" req_enroll ON req_enroll."courseId" = course.id
+WHERE req_enroll."userId" = $1 AND recv.id <> $1
+GROUP BY recv.id
+ORDER BY overlap DESC LIMIT $2 OFFSET $3;
--- server/src/load-env.ts
+++ server/src/load-env.ts
@@ -7,3 +7,13 @@ function main() {
   dotenv.config({ path });
 }
 main();
+
+export function assertLocalDB() {
+  const DATABASE_URL = process.env.DATABASE_URL;
+  if (DATABASE_URL !== "postgres://user:password@localhost:5432/database") {
+    console.log(
+      "SAFETY GUARD: test must be run via `make test`, otherwise it might break dev/prod database",
+    );
+    throw new Error(`got: \`${DATABASE_URL}\``);
+  }
+}
