f237a7d7b18b3cf5620d3cfde6ea75035cf85f84
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-10-03 22:57:45 +0900

    Settings画面を整えた (#363)
    <!--変更したい場合は、`/.github/pull_request_template.md` を修正して下さい。-->
    # PRの概要
    
    <!-- 変更の目的 もしくは 関連する Issue 番号 -->
    <!-- 以下のように書くと Issue にリンクでき、マージ時に自動で Issue を閉じられる-->
    <!-- closes #001 -->
    
    ## 具体的な変更内容
    <!-- ビューの変更がある場合はスクショによる比較などがあると
    かりやすい -->
    
    https://github.com/user-attachments/assets/afaa32c5-7535-4cec-a82e-1fd14dff3fda
    
    
    ## 影響範囲
    <!-- この関数を変更したのでこの機能にも影響がある、など -->
    
    ## 動作要件
    <!-- 動作に必要な 環境変数 / 依存関係 / DBの更新 など -->
    
    ## 補足
    <!-- レビューをする際に見てほしい点、ローカル環境で試す際の注意点、など -->
    
    ## レビューリクエストを出す前にチェック！
    
    - [ ] 改めてセルフレビューしたか
    - [ ] 手動での動作検証を行ったか
    - [ ] server の機能追加ならば、テストを書いたか
      - 理由: 書いた | server の機能追加ではない
    - [ ] 間違った使い方が存在するならば、それのドキュメントをコメントで書いたか
      - 理由: 書いた | 間違った使い方は存在しない
    - [ ] わかりやすいPRになっているか
    
    <!-- レビューリクエスト後は、Slackでもメンションしてお願いすることを推奨します。 -->

---
+++ web/src/routes/tabs/settings/aboutUs.tsx
@@ -0,0 +1,51 @@
+import { ArrowBack, GitHub, Language } from "@mui/icons-material";
+import XIcon from "@mui/icons-material/X";
+import { Box, IconButton, Link, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export default function AboutUs() {
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      sx={{
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        textAlign: "center",
+        position: "relative",
+      }}
+    >
+      <IconButton
+        sx={{ position: "absolute", top: "20px", left: "20px" }}
+        onClick={() => navigate(-1)} // 1つ前のページに戻る
+      >
+        <ArrowBack />
+      </IconButton>
+      <Typography variant="h4" component="h2" gutterBottom>
+        About Us
+      </Typography>
+      <p>
+        ut.code();
+        は、2019年設立の東京大学のソフトウェアエンジニアリングコミュニティです。
+      </p>
+      <p>
+        <Link href="https://utcode.net" target="_blank">
+          <Language /> ウェブサイト
+        </Link>
+      </p>
+      <p>
+        <Link href="https://github.com/ut-code" target="_blank">
+          <GitHub /> ut.code(); の GitHub
+        </Link>
+      </p>
+      <p>
+        <Link href="https://x.com/utokyo_code" target="_blank">
+          <XIcon /> X (旧 Twitter)
+        </Link>
+      </p>
+    </Box>
+  );
+}
