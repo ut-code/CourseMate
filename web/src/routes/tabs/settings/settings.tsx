88e0e51509fbf0671abb645c518309566c525566
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-10-04 18:56:12 +0900

    幅を変更

--- web/src/routes/tabs/settings/settings.tsx
+++ web/src/routes/tabs/settings/settings.tsx
@@ -21,7 +21,7 @@ export default function Settings() {
         justifyContent: "flex-start",
       }}
     >
-      <List sx={{ width: "100%", maxWidth: "500px" }}>
+      <List sx={{ width: "100%" }}>
         <ListItemButton onClick={() => navigate("/settings/profile")}>
           <ListItemText primary="あなたのカード" />
         </ListItemButton>
