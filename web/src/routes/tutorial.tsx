495f4f0ccd64b19a066ea64634c8033136b7b443
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2024-10-08 14:42:40 +0900

    ボタンを追加

--- web/src/routes/tutorial.tsx
+++ web/src/routes/tutorial.tsx
@@ -1,38 +1,50 @@
-import { ArrowBack } from "@mui/icons-material";
-import { Box, IconButton, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, type SwiperClass, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { useState } from "react";
 import Header from "../components/Header";
 
 const tutorialSteps = [
   {
-    label: "気になった人に「いいね」を送ろう！",
+    label: "気になる人に「いいね」を送ろう！",
     imgPath: "/tutorial-1.png",
   },
   {
-    label: "相手からのリクエストが来たら...",
+    label: "リクエストが来たら...",
     imgPath: "/tutorial-2.png",
   },
   {
-    label: "リクエストを承認して、マッチング！",
+    label: "承認して、マッチング！",
     imgPath: "/tutorial-3.png",
   },
   {
-    label: "マッチングした人とチャットをして、仲を深めよう！",
+    label: "チャットを使って仲良くなろう！",
     imgPath: "/tutorial-4.png",
   },
   {
-    label: "カードを編集して、より魅力的なカードを作ろう！",
+    label: "魅力的なカードを作ってね！",
     imgPath: "/tutorial-5.png",
+  },
+  {
+    label: "チュートリアル完了！",
   },
 ];
 
 export default function Tutorial() {
   const navigate = useNavigate();
+  const [isLastSlide, setIsLastSlide] = useState(false);
+
+  const handleSlideChange = (swiper: SwiperClass) => {
+    if (swiper.activeIndex === tutorialSteps.length - 1) {
+      setIsLastSlide(true);
+    } else {
+      setIsLastSlide(false);
+    }
+  };
 
   return (
     <Box
@@ -40,27 +52,12 @@ export default function Tutorial() {
         padding: "20px",
         display: "flex",
         flexDirection: "column",
-        position: "absolute",
-        top: "56px",
-        bottom: 0,
-        left: 0,
-        right: 0,
         overflowY: "auto",
       }}
     >
       <Header title="チュートリアル/Tutorial" />
-      <IconButton
-        sx={{ position: "absolute", top: "20px", left: "20px" }}
-        onClick={() => navigate(-1)}
-      >
-        <ArrowBack />
-      </IconButton>
-
       <Box
         sx={{
-          width: "100%",
-          maxWidth: "600px",
-          padding: "30px",
           textAlign: "left",
         }}
       >
@@ -70,31 +67,48 @@ export default function Tutorial() {
           slidesPerView={1}
           navigation
           pagination={{ clickable: true }}
+          onSlideChange={handleSlideChange}
+          style={{
+            paddingBottom: "60px",
+          }}
         >
           {tutorialSteps.map((step) => (
             <SwiperSlide key={step.imgPath}>
-              <Typography
-                variant="h5"
-                component="h1"
-                gutterBottom
-                sx={{ fontWeight: "bold", mb: "24px", textAlign: "center" }}
-              >
-                {step.label}
-              </Typography>
-              <img
-                src={step.imgPath}
-                alt={step.label}
-                style={{
-                  height: 255,
-                  display: "block",
-                  maxWidth: 400,
-                  overflow: "hidden",
-                  width: "100%",
-                  margin: "0 auto",
-                }}
-              />
+              <Box sx={{ textAlign: "center", mb: "24px" }}>
+                <Typography
+                  variant="h6"
+                  component="h1"
+                  gutterBottom
+                  sx={{ fontWeight: "bold", mb: "16px" }}
+                >
+                  {step.label}
+                </Typography>
+                <img
+                  src={step.imgPath}
+                  alt={step.label}
+                  style={{
+                    display: "block",
+                    width: "60vw",
+                    height: "calc(60vw·*·(667·/·375))",
+                    maxWidth: 400,
+                    overflow: "hidden",
+                    margin: "auto",
+                  }}
+                />
+              </Box>
             </SwiperSlide>
           ))}
+          {isLastSlide && (
+            <Box sx={{ textAlign: "center", mt: "20px" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => navigate("/home")}
+              >
+                ホーム画面へ
+              </Button>
+            </Box>
+          )}
         </Swiper>
       </Box>
     </Box>
