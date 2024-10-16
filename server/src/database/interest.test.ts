b153944bfaf342fe61fd7957abb684c52fb83dd8
aster <137767097+aster-void@users.noreply.github.com>
2024-10-16 19:50:38 +0900

    fix test inconsistency

--- server/src/database/interest.test.ts
+++ server/src/database/interest.test.ts
@@ -5,11 +5,19 @@ import * as interest from "./interest";
 beforeAll(assertLocalDB);
 
 test("list", async () => {
-  expect(await interest.all()).toSatisfy(() => true);
+  const got = (await interest.all()).sort((a, b) => a.id - b.id);
+  expect(got).toEqual([
+    { id: 1, group: "Computer Science", name: "型システム" },
+    { id: 2, group: "Computer Science", name: "機械学習" },
+    { id: 3, group: "Computer Science", name: "CPU アーキテクチャ" },
+    { id: 4, group: "Computer Science", name: "分散処理" },
+    { id: 5, group: "Math", name: "Lean4" },
+  ]);
 });
 
 test("get by user id", async () => {
-  expect(await interest.of(101)).toEqual([
+  const got = (await interest.of(101)).sort((a, b) => a.id - b.id);
+  expect(got).toEqual([
     { id: 1, group: "Computer Science", name: "型システム" },
     { id: 2, group: "Computer Science", name: "機械学習" },
     { id: 3, group: "Computer Science", name: "CPU アーキテクチャ" },
