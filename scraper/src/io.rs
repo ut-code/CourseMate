924ea9c48297441f1dea42b73dabc496d5e38eed
aster <137767097+aster-void@users.noreply.github.com>
2024-10-23 09:19:30 +0900

    refac + revalidate content if file is invalid

--- scraper/src/io.rs
+++ scraper/src/io.rs
@@ -10,24 +10,24 @@ pub async fn write_to(file: &mut fs::File, content: Entry) -> anyhow::Result<()>
     Ok(())
 }
 
-const CACHE_DIR: &str = "./.cache";
+use crate::CACHE_DIR;
 
 pub async fn request(url: &str) -> anyhow::Result<String> {
     println!("[request] sending request to {}", url);
 
     let hash = Sha256::digest(url.as_bytes());
     let path = format!("{CACHE_DIR}/{:x}", hash);
-    match tokio::fs::read(&path).await {
-        Ok(cont) => return Ok(String::from_utf8(cont)?),
-        Err(_) => {
-            // continue
+    if let Ok(content) = fs::read(&path).await {
+        if let Ok(text) = String::from_utf8(content) {
+            return Ok(text);
         }
     }
     let res = reqwest::get(url).await?;
+    let status = res.status().as_u16();
     ensure!(
-        200 <= res.status().as_u16() && res.status().as_u16() < 300,
+        (200..=299).contains(&status),
         "Status check failed: expected 200~299, got {}",
-        res.status()
+        status
     );
     let text = res.text().await?;
     let mut f = tokio::fs::File::create(path).await?;
