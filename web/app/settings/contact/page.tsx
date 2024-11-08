028b3b8122cfe2a3f33878c7499bc1eaf83d0d53
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-11-09 01:56:14 +0900

    アイコン導入etc

--- web/app/settings/contact/page.tsx
+++ web/app/settings/contact/page.tsx
@@ -1,28 +1,15 @@
-import { Box, Typography } from "@mui/material";
 import { NavigateByAuthState } from "~/components/common/NavigateByAuthState";
 import TopNavigation from "~/components/common/TopNavigation";
 
 export default function Contact() {
   return (
     <NavigateByAuthState type="toLoginForUnauthenticated">
-      <Box
-        sx={{
-          padding: "8px",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
+      <div className="flex flex-col p-2">
         <TopNavigation title="お問い合わせ" />
-        <Box
-          sx={{
-            width: "100%",
-            padding: "30px",
-            textAlign: "left",
-          }}
-        >
-          <Typography sx={{ mb: "16px", lineHeight: "1.8" }}>
+        <div className="w-full p-8 text-left">
+          <p className="mb-4 leading-7">
             ご利用いただきありがとうございます。サービスに関するご意見やバグ報告がございましたら、以下のリンクからお問い合わせください。皆様のフィードバックは、サービスの改善に役立てさせていただきます。
-          </Typography>
+          </p>
           <a
             href="https://forms.gle/WvFTbsJoHjGp9Qt88"
             target="_blank"
@@ -31,8 +18,8 @@ export default function Contact() {
           >
             ご意見・バグ報告をする
           </a>
-        </Box>
-      </Box>
+        </div>
+      </div>
     </NavigateByAuthState>
   );
 }
