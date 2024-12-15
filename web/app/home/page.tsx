3474fc64db9c1d7cc7f607e7170c526d82d2f8c7
GuY8528 <125722953+GuY8528@users.noreply.github.com>
2024-12-15 15:10:17 +0900

    recommendedの型修正

--- web/app/home/page.tsx
+++ web/app/home/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import CloseIcon from "@mui/icons-material/Close";
-import type { User } from "common/types";
+import { UserWithCoursesAndSubjects } from "common/types";
 import { motion, useAnimation } from "framer-motion";
 import { useCallback, useEffect, useState } from "react";
 import { MdThumbUp } from "react-icons/md";
@@ -21,7 +21,7 @@ export default function Home() {
   } = useMyID();
 
   const [_, rerender] = useState({});
-  const [recommended, setRecommended] = useState<Queue<User>>(
+  const [recommended, setRecommended] = useState<Queue<UserWithCoursesAndSubjects>>(
     () => new Queue([]),
   );
   useEffect(() => {
