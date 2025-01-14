62a10ce4da259f04456e838e77cd0bc4d10bd95c
aster <137767097+aster-void@users.noreply.github.com>
2025-01-14 22:26:27 +0900

    fix type error

--- server/src/functions/engines/recommendation.ts
+++ server/src/functions/engines/recommendation.ts
@@ -1,8 +1,7 @@
 import { recommend as sql } from "@prisma/client/sql";
 import { Err, Ok, type Result } from "common/lib/result";
-import type { UserID, UserWithCoursesAndSubjects } from "common/types";
+import type { User, UserID, UserWithCoursesAndSubjects } from "common/types";
 import { prisma } from "../../database/client";
-import { getUserByID } from "../../database/users";
 
 export async function recommendedTo(
   user: UserID,
@@ -11,8 +10,8 @@ export async function recommendedTo(
 ): Promise<
   Result<
     Array<{
-      u: UserWithCoursesAndSubjects;
-      overlap: number;
+      u: User; // UserWithCoursesAndSubjects
+      count: number;
     }>
   >
 > {
@@ -20,9 +19,10 @@ export async function recommendedTo(
     const result = await prisma.$queryRawTyped(sql(user, limit, offset));
     return Promise.all(
       result.map(async (res) => {
-        if (!res) throw new Error("res is null: something is wrong");
         const { overlap: count, ...u } = res;
-        return { count, u };
+        if (count === null)
+          throw new Error("count is null: something is wrong");
+        return { count: Number(count), u };
       }),
     )
       .then((val) => Ok(val))
