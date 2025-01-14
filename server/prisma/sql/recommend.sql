63032ab82a75ad8020a0f200586190829a8fa738
aster <137767097+aster-void@users.noreply.github.com>
2025-01-14 21:42:30 +0900

    fix the SQL

--- server/prisma/sql/recommend.sql
+++ server/prisma/sql/recommend.sql
@@ -1,28 +1,30 @@
+-- $1 = senderId
 SELECT
-    $1 AS senderId,
     recv, 
+    -- course overlap
     (SELECT COUNT(1) FROM "Course" course
-        WHERE EXISTS (SELECT 1 FROM "Enrollment" e WHERE e.userId = recv.id)
-        AND EXISTS (SELECT 1 FROM "Enrollment" e WHERE e.userId = senderId)
-    ) AS course_overlap,
+        WHERE EXISTS (SELECT 1 FROM "Enrollment" e WHERE e."courseId" = course.id AND e."userId" = recv.id)
+        AND EXISTS (SELECT 1 FROM "Enrollment" e WHERE e."courseId" = course.id AND e."userId" = $1)
+    )
+    + -- interest overlap
     (SELECT COUNT(1) FROM "InterestSubject" subj
-        WHERE EXISTS (SELECT 1 FROM "Interest" i WHERE i.userId = recv.id)
-        AND EXISTS (SELECT 1 FROM "Interest" i WHERE i.userId = senderId)
-    ) AS interest_overlap
+        WHERE EXISTS (SELECT 1 FROM "Interest" i WHERE i."subjectId" = subj.id AND i."userId" = recv.id)
+        AND EXISTS (SELECT 1 FROM "Interest" i WHERE i."subjectId" = subj.id AND i."userId" = $1)
+    ) AS overlap
 FROM "User" recv
-WHERE recv.id <> senderId
+WHERE recv.id <> $1
 
 AND NOT EXISTS (
     SELECT 1 FROM "Relationship" rel
-    WHERE rel."sendingUserId" IN (senderId, recv.id) AND rel."receivingUserId" IN (senderId, recv.id)
+    WHERE rel."sendingUserId" IN ($1, recv.id) AND rel."receivingUserId" IN ($1, recv.id)
     AND (status = 'MATCHED' OR status = 'REJECTED')
 )
 
 AND NOT EXISTS (
     SELECT 1 FROM  "Relationship" rel_pd
-    WHERE rel_pd."sendingUserId" = senderId AND rel_pd."receivingUserId" = recv.id
+    WHERE rel_pd."sendingUserId" = $1 AND rel_pd."receivingUserId" = recv.id
     AND status = 'PENDING'
 )
 
-ORDER BY course_overlap + interest_overlap DESC
+ORDER BY overlap DESC
 LIMIT $2 OFFSET $3;
