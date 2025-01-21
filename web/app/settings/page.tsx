bdebbd0c87b9880972b18cebc9038518dd0f8f3f
Yuki Kobayashi <137767097+aster-void@users.noreply.github.com>
2025-01-21 22:26:33 +0900

    fixed 608: it was because it was loading auth state. (#613)
    closes #608

--- web/app/settings/page.tsx
+++ web/app/settings/page.tsx
@@ -24,29 +24,27 @@ function Item({
 
 export default function Settings() {
   return (
-    <NavigateByAuthState type="toLoginForUnauthenticated">
-      <div className="flex flex-col justify-start">
-        <h2 className="p-4 pb-2 text-gray-500 text-xs">基本情報</h2>
-        <ul className="w-full">
-          <Item href="/settings/profile" title="プロフィール" />
-          <Item href="/settings/courses" title="授業" />
-          <Item href="/settings/interests" title="興味分野" />
-          <Item href="/settings/card" title="カードのプレビュー" />
-        </ul>
-        <h2 className="p-4 py-2 text-gray-500 text-xs">その他</h2>
-        <ul className="w-full">
-          <Item href="/tutorial" title="CourseMateの使い方" />
-          <Item href="/settings/notification" title="運営からのお知らせ" />
-          <Item href="/settings/contact" title="お問い合わせ" />
-          <Item href="/faq" title="よくあるご質問" />
-          <Item href="/settings/aboutUs" title="About Us" />
-          <Item href="/settings/disclaimer" title="免責事項" />
-          <Item href="/settings/delete" title="アカウント削除" />
-          <li>
-            <LogOutButton />
-          </li>
-        </ul>
-      </div>
-    </NavigateByAuthState>
+    <div className="flex flex-col justify-start">
+      <h2 className="p-4 pb-2 text-gray-500 text-xs">基本情報</h2>
+      <ul className="w-full">
+        <Item href="/settings/profile" title="プロフィール" />
+        <Item href="/settings/courses" title="授業" />
+        <Item href="/settings/interests" title="興味分野" />
+        <Item href="/settings/card" title="カードのプレビュー" />
+      </ul>
+      <h2 className="p-4 py-2 text-gray-500 text-xs">その他</h2>
+      <ul className="w-full">
+        <Item href="/tutorial" title="CourseMateの使い方" />
+        <Item href="/settings/notification" title="運営からのお知らせ" />
+        <Item href="/settings/contact" title="お問い合わせ" />
+        <Item href="/faq" title="よくあるご質問" />
+        <Item href="/settings/aboutUs" title="About Us" />
+        <Item href="/settings/disclaimer" title="免責事項" />
+        <Item href="/settings/delete" title="アカウント削除" />
+        <li>
+          <LogOutButton />
+        </li>
+      </ul>
+    </div>
   );
 }
--- web/app/tutorial/layout.tsx
+++ web/app/tutorial/layout.tsx
@@ -2,20 +2,22 @@ import Link from "next/link";
 import Header from "~/components/Header";
 import { NavigateByAuthState } from "~/components/common/NavigateByAuthState";
 
-export default function TutorialPageLayout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <NavigateByAuthState type="toLoginForUnauthenticated">
+    <>
       <Header title="チュートリアル/Tutorial" />
-      <div className="flex-1">{children}</div>
+      <NavigateByAuthState type="toLoginForUnauthenticated">
+              <div className="flex-1">{children}</div>
+      </NavigateByAuthState>
       <div className="m-4 text-center">
         <Link href="/home" className="btn btn-primary w-full">
           ホーム画面へ
         </Link>
       </div>
-    </NavigateByAuthState>
+    </>
   );
 }
