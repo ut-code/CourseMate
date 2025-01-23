affe0486284967878f40ba8c499e856f6a4ffb78
naka-12 <104970808+naka-12@users.noreply.github.com>
2025-01-23 17:10:09 +0900

    temporary fix

--- server/src/functions/engines/recommendation.ts
+++ server/src/functions/engines/recommendation.ts
@@ -4,6 +4,7 @@ import type { UserID, UserWithCoursesAndSubjects } from "common/types";
 import { prisma } from "../../database/client";
 import { getCoursesByUserId } from "../../database/courses";
 import * as interest from "../../database/interest";
+import { getUserByID } from "../../database/users";
 
 export async function recommendedTo(
   user: UserID,
@@ -24,12 +25,15 @@ export async function recommendedTo(
         const { overlap: count, ...u } = res;
         if (count === null)
           throw new Error("count is null: something is wrong");
+        // TODO: user の情報はここで再度 DB に問い合わせるのではなく、 recommend の sql で取得
+        const user = await getUserByID(u.id);
+        if (!user.ok) throw new Error("user not found");
         const courses = getCoursesByUserId(u.id);
         const subjects = interest.of(u.id);
         return {
           count: Number(count),
           u: {
-            ...u,
+            ...user.value,
             courses: await courses,
             interestSubjects: await subjects,
           },
