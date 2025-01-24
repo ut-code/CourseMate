1bb7f859dbadbfa15c7a47ba5811f8c8aebb8407
aster <137767097+aster-void@users.noreply.github.com>
2025-01-24 23:19:29 +0900

    fix cmt

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // FIXME!! FIXME!!!!!! TODO!!! you haven't even enabled it smh :/
+  // FIXME!! FIXME!!!!!! TODO!!! this hasn't even been enabled smh :/
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
