d3b1d637ca5b04bbe786a6fb87f4c11ffd0ed95c
aster <137767097+aster-void@users.noreply.github.com>
2025-01-26 21:21:09 +0900

    run type check on build

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -6,15 +6,6 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-
-  // fuck you
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
