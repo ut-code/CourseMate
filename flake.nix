c9d19ce0eb24468286a426c87609d5a861159897
aster <137767097+aster-void@users.noreply.github.com>
2025-02-01 19:00:04 +0900

    install npm and update package-lock.json

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
           nix # HACK: to fix the side effect of the hack below, installing two instances of nix
           gnumake
           bun
-          nodejs-slim
+          nodejs
           biome
           lefthook
           dotenv-cli
