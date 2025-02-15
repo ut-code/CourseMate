349bb273ed6f0df6f3e245a91c0fbafa35f0c095
Shogo Nakamura <104970808+naka-12@users.noreply.github.com>
2025-02-15 19:12:54 +0900

    SQL のログを出力 (#648)
    # PRの概要
    
    - SQL のログを server のコンソールに出せるようにした

--- server/src/database/client.ts
+++ server/src/database/client.ts
@@ -1,9 +1,36 @@
 import { PrismaClient } from "@prisma/client";
+import { format } from "sql-formatter";
 import "../load-env";
 
-export let prisma = new PrismaClient();
+const { SQL_LOG } = process.env;
 
-// not sure if this is necessary
-export function reload() {
-  prisma = new PrismaClient();
-}
+export const prisma = new PrismaClient(
+  SQL_LOG === "true"
+    ? {
+        log: [
+          {
+            emit: "event",
+            level: "query",
+          },
+          {
+            emit: "stdout",
+            level: "error",
+          },
+          {
+            emit: "stdout",
+            level: "info",
+          },
+          {
+            emit: "stdout",
+            level: "warn",
+          },
+        ],
+      }
+    : undefined,
+);
+
+prisma.$on("query", (e) => {
+  console.log(format(e.query, { language: "postgresql" }));
+  console.log(`Params: ${e.params}`);
+  console.log("\n");
+});
