8eda77fa978e9961a1f73be032b77664f08cda8d
Shogo Nakamura <104970808+naka-12@users.noreply.github.com>
2025-04-12 02:08:40 +0900

    experiemntal: safe area (#690)

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         <link rel="icon" type="image/svg+xml" href="/course-mate-icon.svg" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
         <title>CourseMate</title>
       </head>
