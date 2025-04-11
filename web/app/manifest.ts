f8fbcaf015551ac02b513428c0cb205074ecab9d
naka-12 <104970808+naka-12@users.noreply.github.com>
2025-04-12 00:05:01 +0900

    fix: theme color を白に

--- web/app/manifest.ts
+++ web/app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#000000",
+    theme_color: "#ffffff",
     icons: [
       {
         src: "/icon-192x192.png",
